corrected rotation curve

SETI.py
@@ -71,6 +71,7 @@ N_gal = N_disk + N_bulge
 R_opt = 2.*h_rho_disk # (pc)
 V_opt = 300./pc # (pc/s)
 n_s_gal = 4
+L2Lstar = 0.1
 
 #    Initialisation (t=0)    #
 # ========================== #
@@ -100,7 +101,8 @@ z_bulge = r_bulge*np.cos(phi_bulge)
 phi_bulge = theta_bulge
 
 # VEL:Disk (Rotation curve analytic relation)
-V_disk = tools.v_r_curve(rho_disk, VH, aH)
+#V_disk = tools.v_r_curve(rho_disk, VH, aH)
+V_disk = tools.v_rotational(rho_disk, V_opt, R_opt, L2Lstar)
 
 # VEL:Bulge (Gaussian dist. with given mean and dispersion)
 V_bulge = tools.init_norm(mean_bulge, sigma_bulge, N_bulge)
@@ -156,35 +158,38 @@ galaxy_cart2[1,:] = galaxy[0,:]*np.sin(galaxy[1,:])
 
 def update():
     global t
-    with open('logfile.txt', 'w') as logfile:
-    #    UPDATING (t=t+dt)    #
-    # ======================= #
-        count = 1
-        col_tot = 0.
-        count_tot = 0.
-        logg = np.zeros(((int(t_f/dt)+1), 4))
-        for i in xrange(int(t_f/dt)+1):
-            t = i*dt
-            print '\n-----------------\nt = %f Myr\n-----------------'%(t/Myr)
-            logg[i, 0] = t
-        # Colonize the galaxy!
-            dist = VC * (dt-dt_const)    # (pc)
-            galaxy[4,:], galaxy[5,:], colonized, count = tools.col_single(galaxy, dist, count)
-#            ind = np.where(CS_gal==1)[0]
-#            galaxy[4,:], galaxy[5,:], colonized, count = tools.col_inf(galaxy, dist, count, ind)
-            col_tot += colonized
-            count_tot += count
-            logg[i, 1] = col_tot
-            logg[i, 2] = np.sum(galaxy[4,:])
-            logg[i, 3] = np.sum(abs(galaxy[5,:]))-1
-        # Rotate the galaxy!
-            galaxy[1,0:N_disk] += np.arcsin(galaxy[3,0:N_disk]*dt/galaxy[0,0:N_disk])
-            galaxy_cart2[0,:] = galaxy[0,:]*np.cos(galaxy[1,:])
-            galaxy_cart2[1,:] = galaxy[0,:]*np.sin(galaxy[1,:])
-#            if t%Myr==0:
+    logfile = open('logfile.txt', 'w')
+#    with open('logfile.txt', 'w') as logfile:
+#    UPDATING (t=t+dt)    #
+# ======================= #
+    count = 1
+    col_tot = 0.
+    count_tot = 0.
+    logg = np.zeros(((int(t_f/dt)+1), 4))
+    for i in xrange(int(t_f/dt)+1):
+        t = i*dt
+#        print '\n-----------------\nt = %f Myr\n-----------------'%(t/Myr)
+        logg[i, 0] = t
+    # Colonize the galaxy!
+        dist = VC * (dt-dt_const)    # (pc)
+#        galaxy[4,:], galaxy[5,:], colonized, count = tools.col_single(galaxy, dist, count)
+        ind = np.where(CS_gal==1)[0]
+        galaxy[4,:], galaxy[5,:], colonized, count = tools.col_inf(galaxy, dist, count, ind)
+        col_tot += colonized
+        count_tot += count
+        logg[i, 1] = col_tot
+        logg[i, 2] = np.sum(galaxy[4,:])
+        logg[i, 3] = np.sum(abs(galaxy[5,:]))+1
+    # Rotate the galaxy!
+        galaxy[1,0:N_disk] += np.arcsin(galaxy[3,0:N_disk]*dt/galaxy[0,0:N_disk])
+        galaxy_cart2[0,:] = galaxy[0,:]*np.cos(galaxy[1,:])
+        galaxy_cart2[1,:] = galaxy[0,:]*np.sin(galaxy[1,:])
+        if i%(10)==0:
             logfile.write('%e\t%e\t%e\t%e\n' %(logg[i,0], logg[i,1], logg[i,2], logg[i,3]))
+            print '\n-----------------\nt = %f Myr\n-----------------'%(t/Myr)
 
-            i += 1
+        i += 1
+
 
     logfile.close()
     #       TIMING          #

tools.py
@@ -57,10 +57,19 @@ def    init_norm(loc, scale, size):
     return init
 
 # Calculating velocity as a function of r to force the rotation curve (A)
-def v_r_curve(r, VH, aH):
-    V = VH*np.sqrt(1.-(aH/r)*np.arctan(r/aH))
-    #Not the most accurate, but easiest!! Consider the Universal SSP instead...
-    return V
+def v_rotational(r, V_opt, R_opt, L2Lstar):
+    V2_opt = np.power(V_opt, 2)
+    x = r/R_opt
+    a = 1.5*np.power(L2Lstar, 1.5)
+    beta = 0.72+0.44*np.log10(L2Lstar)
+    # Disk component
+    V2_disk = V2_opt*beta*1.97*np.power(x, 1.22)/np.power((x**2+0.78**2), 1.43)
+    # DM component
+    V2_DM = V2_opt*(1. - beta)*(1. + a**2)*x**2/(x**2 + a**2)
+
+    V2_rot = V2_disk + V2_DM
+    return np.sqrt(V2_rot)
+
 
 #Initializing disk luminosity distribution
 def init_sersic(I_0, alpha, n_s, r):
@@ -141,6 +150,7 @@ def col_single(galaxy, dist, count):
         while col_dist < dist/2.:
             galaxy, dmin = update_colonization(galaxy, dist, ind,
                                                reachable)
+            
             colonized += 1
 #            print '%d newly colonized'%(colonized)
 #            print '%f pc to go!'%(dist-col_dist)
@@ -189,15 +199,17 @@ def update_colonization(galaxy, dist, ind, reachable):
         d2[0,reachable] = np.power(galaxy_cart2[0,reachable]-x_col,2)
         d2[0,reachable] += np.power(galaxy_cart2[1,reachable]-y_col,2)
         d2[0,reachable] += np.power(galaxy[2,reachable]-z_col,2)
+#        print 'size(d2):%d'%(np.size(d2))
         d2min = d2[d2 !=0].min()
-#        d2min = np.min(d2[np.nonzero(d2)])
+#        print d2min
         dmin = np.sqrt(d2min)
         ind_dmin = np.where(d2==d2min)[1]
-#        print'Mission accomplished at # %d: '%(ind_dmin[0])
-#        print '(d_min, r_reachable) = (%f, %f)'%(dmin, dist)
         galaxy[5,ind] = -1.
         galaxy[5,ind_dmin] = 1.
         galaxy[4, ind_dmin] = 0
+#        d2min = np.min(d2[np.nonzero(d2)])
+#        print'Mission accomplished at # %d: '%(ind_dmin[0])
+#        print '(d_min, r_reachable) = (%f, %f)'%(dmin, dist)
         return galaxy, dmin
 
 