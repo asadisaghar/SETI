disk and all rotations ok, bulge and halo need fixing with initial dist. and rotation

SETI.py
@@ -39,15 +39,15 @@ cSpeed = 3.e5 #[km/s]
 # ===================== #
 #        HANDLES        #
 # ===================== #
-N_disk = int(5.e2)  # number of particles in disk (same order as N_gal)
+N_disk = int(5.e3)  # number of particles in disk (same order as N_gal)
 #dt = np.logspace(-1, 1, num=1)*Myr  # galactic rotation time step
-dt = 0.5*Myr2sec #[s] #do NOT use anything longer than 0.1 Myr, or disk rotation is messed up!!
+dt = 0.1*Myr2sec #[s] #do NOT use anything longer than 0.1 Myr, or disk rotation is messed up!!
 #dt_const = np.logspace(-12, 1, num=1)*Myr  # construction time delay
 dt_const = 1e-12*Myr2sec #[s]
 #VC = np.logspace(2, 0, num=1)*cSpeed  # probe velocity
 VC = 1e-3*cSpeed #[km/s]
 t = 1
-t_f = 5.e3*Myr2sec  # time to stop #[s]
+t_f = 5.e2*Myr2sec  # time to stop #[s]
 SingleProbe = False
 InfiniteProbe = not(SingleProbe)
 coveringFraction = 1.0
@@ -75,8 +75,8 @@ mean_z_disk = 30.  #[km/s]
 sigma_z_disk = 15.  #[km/s]
 
 # BULGE
-N_bulge = int(0.33*N_disk)
-#N_bulge = 0
+#N_bulge = int(0.33*N_disk)
+N_bulge = 0
 R_bulge = 2.7e3  # bulge radius #[pc]
 I_0_bulge = 5.e9  # bulge central intensity
 alpha_bulge = R_bulge/3.  # bulge scale length #[pc]
@@ -87,7 +87,8 @@ mean_bulge = 100. # (My arbitrary value!!) #FIXME value  #[km/s]
 sigma_bulge = 50. # Velocity dispercion  #[km/s]
 
 # Halo (NOT included!)
-N_halo = int(0.33*N_disk)
+#N_halo = int(0.33*N_disk)
+N_halo = 0
 R_halo = 1.e8
 I_0_halo = 5.e6  # bulge central intensity
 alpha_halo = R_halo/3.  # bulge scale length
@@ -114,7 +115,7 @@ phi_thin = tools.init_pos(N_thin, 0., 2*pi, 'uni')  #[pc]
 z_thin = tools.init_z(N_thin, 0., h_z_thin, 'exp')  #[pc]
 
 # POS:Thick disk (Cylindrical)
-rho_thick = tools.init_pos(N_thick, 0, h_rho_disk, 'exp')  #[pc]
+rho_thick = tools.init_pos(N_thick, 0., h_rho_disk, 'exp')  #[pc]
 phi_thick = tools.init_pos(N_thick, 0., 2*pi, 'uni')  #[pc]
 z_thick = tools.init_z(N_thick, 0., h_z_thick, 'exp')  #[pc]
 
@@ -212,7 +213,7 @@ i = 0
 #count_tot = 0.
 colonized_fraction = np.sum(galaxy[5])/len(galaxy[5])
 
-while abs(colonized_fraction) < 0.7 and t < t_f:
+while abs(colonized_fraction) < 0.7 or t < t_f:
 #    print t
     t = i*dt #[sec]
     # Colonize the galaxy!
@@ -236,15 +237,19 @@ while abs(colonized_fraction) < 0.7 and t < t_f:
     galaxy[1,:N_bulge] += galaxy[3,:N_bulge]*km2pc*dt/galaxy[0,:N_bulge] # v_theta = r*dtheta/dt  #[pc]
     galaxy[2,:N_bulge] += galaxy[7,:N_bulge]*km2pc*dt/galaxy[0,:N_bulge]*np.sin(galaxy[2,:N_bulge]) # v_phi = r*sin(theta)*dphi/dt #[pc]
     # Evaluate disk velocities (Cylindrical)
-    sign = np.round(np.random.uniform(0,1,N_disk))*2.-1
-    galaxy[6,N_bulge:N_disk+N_bulge] = sign*np.random.normal(mean_rho_disk, sigma_rho_disk, N_disk) #[km/s]
+#    sign = np.round(np.random.uniform(0,1,N_disk))*2.-1
+#    galaxy[6,N_bulge:N_disk+N_bulge] = sign*np.random.normal(mean_rho_disk, sigma_rho_disk, N_disk) #[km/s]
     galaxy[3,N_bulge:N_disk+N_bulge] = tools.v_rotational(galaxy[0,N_bulge:N_disk+N_bulge]*pc2km, V_opt, R_opt*pc2km, L2Lstar) #[km/s]
-    sign = np.round(np.random.uniform(0,1,N_disk))*2.-1
-    galaxy[7,N_bulge:N_disk+N_bulge] = sign*np.random.normal(mean_z_disk, sigma_z_disk, N_disk) #[km/s]
+#    sign = np.round(np.random.uniform(0,1,N_disk))*2.-1
+#    galaxy[7,N_bulge:N_disk+N_bulge] = sign*np.random.normal(mean_z_disk, sigma_z_disk, N_disk) #[km/s]
     # Rotate the disk
-    galaxy[0,N_bulge:N_disk+N_bulge] += galaxy[6,N_bulge:N_disk+N_bulge]*dt*km2pc # v_rho = drho_dt #[pc]
+#    galaxy[0,N_bulge:N_disk+N_bulge] += galaxy[6,N_bulge:N_disk+N_bulge]*dt*km2pc # v_rho = drho_dt #[pc]
     galaxy[1,N_bulge:N_disk+N_bulge] += galaxy[3,N_bulge:N_disk+N_bulge]*km2pc*dt/galaxy[0,N_bulge:N_disk+N_bulge] # v_phi = rho*dphi/dt #[pc]
-    galaxy[2,N_bulge:N_disk+N_bulge] += galaxy[7,N_bulge:N_disk+N_bulge]*dt*km2pc # v_z = dz/dt #[pc]
+#    galaxy[2,N_bulge:N_disk+N_bulge] += galaxy[7,N_bulge:N_disk+N_bulge]*dt*km2pc # v_z = dz/dt #[pc]
+    # Oscillate disk particles (around original rho and z positions only)
+    #FIXME: Why add the initial state and not the previous step? Why amp \sim h_z_thick?
+    galaxy[0,N_bulge:N_disk+N_bulge] = rho_disk + tools.r_oscillation(galaxy, t, mean_rho_disk, N_bulge, N_disk, amp=h_z_thick)
+    galaxy[2,N_bulge:N_disk+N_bulge] = tools.z_oscillation(galaxy, t, mean_z_disk, N_bulge, N_disk, amp=z_disk)    
     # Evaluate halo velocities (Spherical)
     sign = np.round(np.random.uniform(0,1,N_halo))*2.-1
     galaxy[6,N_bulge+N_disk:] = sign*np.random.normal(mean_halo, sigma_halo, N_halo) #[km/s]
@@ -262,15 +267,15 @@ while abs(colonized_fraction) < 0.7 and t < t_f:
 
 #    if np.round(colonized_fraction)==0.2 or np.round(colonized_fraction)==0.5:
     print "%.1f Myr \t %.2f colonized"%(t*sec2Myr, colonized_fraction*100.)
-    if (t*sec2Myr)%5 == 1:
+    if (t*sec2Myr)%5 == 0:
         print "%.2f colonized"%(colonized_fraction)
         print "Writing to file..."
-        filename = "galaxy_%2.0f"%(t*sec2Myr)
+        filename = "galaxy_%.2d"%(int(t*sec2Myr))
         np.save(filename, galaxy)
 
 
 print "%.2f colonized"%(colonized_fraction)
 print "Writing to file..."
-filename = "galaxy_%2.0f"%(t*sec2Myr)
+filename = "galaxy_%.2d"%(int(t*sec2Myr))
 np.save(filename, galaxy)
 #cProfile.run("update()", "stats")

tools.py
@@ -105,9 +105,20 @@ def v_rotational(r, V_opt, R_opt, L2Lstar):
     V2_rot = V2_disk + V2_DM
     return np.sqrt(V2_rot)
 
-# Calculating radial velocity od the disk as a function of r from arXiv:1104.3236v4 (eq. 12) 
-def v_radial(r, R_c):
-    return 1. - np.exp(-r/R_c)
+# Calculating the oscillatory motion of disk particles [along z direction]
+def z_oscillation(galaxy, t, v, N_bulge, N_disk, amp):
+    z = galaxy[2,N_bulge:N_bulge+N_disk]
+    omega = 2.*pi/(25.*pi*1e7*1e6)
+    phase = init_pos(N_disk, 0., 2.*pi, 'uni')
+    galaxy[2,N_bulge:N_bulge+N_disk] = amp*np.cos(omega*t+phase)
+    return galaxy[2,N_bulge:N_bulge+N_disk]
+
+def r_oscillation(galaxy, t, v, N_bulge, N_disk, amp):
+    r = galaxy[0,N_bulge:N_bulge+N_disk]
+    omega = 2.*pi/(25.*pi*1e7*1e6)
+    phase = init_pos(N_disk, 0., 2.*pi, 'uni')
+    galaxy[0,N_bulge:N_bulge+N_disk] = amp*np.cos(omega*t+phase)
+    return galaxy[0,N_bulge:N_bulge+N_disk]
 
 #Initializing disk luminosity distribution
 def init_sersic(I_0, alpha, n_s, r):